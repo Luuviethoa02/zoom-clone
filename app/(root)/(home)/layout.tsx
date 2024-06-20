e21fc8775a1a9acfacec010d807c1e6d24d20b7d
Luuhoaaa1212 <hahahawe1239@gmail.com>
2024-06-20 18:10:02 +0700

    feat:project init

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,32 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling application",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
+const HomeLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <main className="relative">
+      <Navbar/>
+      <div className="flex">
+       <Sidebar/>
+        <section className="flex flex-1 min-h-screen flex-col px-6 py-28 pb-6 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default HomeLayout;
---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+export const sideBarLinks = [
+  {
+    lable: "Home",
+    route: "/",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    lable: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    lable: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+
+  {
+    lable: "Recording",
+    route: "/recording",
+    imgUrl: "/icons/Video.svg",
+  },
+  {
+    lable: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
+];
